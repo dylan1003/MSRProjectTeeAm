d48cea5cf02c922925ea2fa47a78f1f78952161a
Jackson Hales <me@jacksonhales.com.au>
2018-10-31 09:23:37 +1100

    Initial commit on branch

---
+++ MSR_View_Login/Models/PortfolioCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MSR_mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimplifiedLogin.Models
+{
+    public class PortfolioCreateViewModel
+    {
+        public Veteran Veteran { get; set; }
+        public List<Section> Sections { get; set; }
+        public List<Content> Contents { get; set; }
+        public Section section { get; set; }
+        public Content content { get; set; }
+
+    }
+}
