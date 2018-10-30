87413d1769275c78122064378f75e6e41adcb251
Jackson Hales <me@jacksonhales.com.au>
2018-10-31 10:19:53 +1100

    Added new ADO EF model

--- MSR_View_Login/Controllers/AdminController.cs
+++ MSR_View_Login/Controllers/AdminController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MSR_mvc.Models;
 using Microsoft.AspNet.Identity;
 
 namespace SimplifiedLogin.Controllers
