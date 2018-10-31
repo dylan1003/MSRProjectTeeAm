4cbd68d2215d90ac02a1952f5c8bc624b4be30df
Jackson Hales <me@jacksonhales.com.au>
2018-10-31 14:21:48 +1100

    Portfolio Create page added, UserVeteran controller updated.

--- MSR_View_Login/Controllers/UserVeteranController.cs
+++ MSR_View_Login/Controllers/UserVeteranController.cs
@@ -12,15 +12,85 @@ namespace MSR_Web_App.Controllers
     public class UserVeteranController : Controller
     {
         private Msr_Database_Release_TwoEntities db = new Msr_Database_Release_TwoEntities();
+        PortfolioCreateViewModel viewModel = new PortfolioCreateViewModel();
+
         // GET: Veteran
-        public ActionResult Index()
+        public ActionResult Portfolio()
         {
-            return View();
+            int userID = Convert.ToInt32(User.Identity.GetUserName());
+            // Get veteran from login info 
+            viewModel.Veteran = db.Veterans.SingleOrDefault(v => v.Id == userID);
+            viewModel.Sections = db.Sections.Where(s => s.Veteran.Id ==userID).ToList();
+
+            return View("Portfolio", viewModel);
         }
-        
+
         public ActionResult Manage()
         {
             return View();
         }
+
+        public ActionResult AddSection(Section section)
+        {
+            // get ALL current sections
+            viewModel.Sections = db.Sections.ToList();
+
+            // find next Id for section (largest)
+            int nextId = 0;
+            foreach (Section s in viewModel.Sections)
+            {
+                if (s.Id > nextId)
+                {
+                    nextId = s.Id;
+                }
+
+            }
+
+            section.Id = nextId + 1;
+
+            db.Sections.Add(section);
+            db.SaveChanges();
+
+            return RedirectToAction("Portfolio", viewModel);
+        }
+
+        public ActionResult AddContent(Content content)
+        {
+
+            // get current sections
+            viewModel.Sections = db.Sections.ToList();
+
+            Section currentSection = db.Sections.SingleOrDefault(s => s.Id == content.Section.Id);
+            content.Section.Id = currentSection.Id;
+
+            // find next Id for section (largest)
+            int nextId = 0;
+            foreach (Content c in currentSection.Contents)
+            {
+                if (c.Id > nextId)
+                {
+                    nextId = c.Id;
+                }
+
+            }
+
+            content.Id = nextId + 1;
+
+            db.Contents.Add(content);
+            db.SaveChanges();
+
+            return RedirectToAction("Portfolio", viewModel);
+        }
+
+        public ActionResult DeleteContent(Content content)
+        {
+
+            Content contentToDelete = db.Contents.SingleOrDefault(c => c.Id == content.Id && c.Section.Id == content.Section.Id);
+            db.Contents.Remove(contentToDelete);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", viewModel);
+        }
+
     }
 }
