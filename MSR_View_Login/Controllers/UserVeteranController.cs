1bce996a9edfc8c86e75c9d0182e20c476f819f1
Jackson Hales <me@jacksonhales.com.au>
2018-11-01 14:31:49 +1100

    More work on create page and controller

--- MSR_View_Login/Controllers/UserVeteranController.cs
+++ MSR_View_Login/Controllers/UserVeteranController.cs
@@ -15,14 +15,14 @@ namespace MSR_Web_App.Controllers
         PortfolioCreateViewModel viewModel = new PortfolioCreateViewModel();
 
         // GET: Veteran
-        public ActionResult Portfolio()
+        public ActionResult PortfolioCreation()
         {
             int userID = Convert.ToInt32(User.Identity.GetUserName());
             // Get veteran from login info 
             viewModel.Veteran = db.Veterans.SingleOrDefault(v => v.Id == userID);
             viewModel.Sections = db.Sections.Where(s => s.Veteran.Id ==userID).ToList();
 
-            return View("Portfolio", viewModel);
+            return View("PortfolioCreation", viewModel);
         }
 
         public ActionResult Manage()
@@ -51,7 +51,7 @@ namespace MSR_Web_App.Controllers
             db.Sections.Add(section);
             db.SaveChanges();
 
-            return RedirectToAction("Portfolio", viewModel);
+            return RedirectToAction("PortfolioCreation", viewModel);
         }
 
         public ActionResult AddContent(Content content)
@@ -59,9 +59,13 @@ namespace MSR_Web_App.Controllers
 
             // get current sections
             viewModel.Sections = db.Sections.ToList();
+            int vetId = Int32.Parse(User.Identity.GetUserName());
+            Veteran veteran = db.Veterans.SingleOrDefault(v => v.Id == vetId);
+            Section currentSection = db.Sections.SingleOrDefault(s => s.Id == content.FK_Section_Id);
+            //content.FK_Section_Id = currentSection.Id;
+            content.Section = currentSection;
 
-            Section currentSection = db.Sections.SingleOrDefault(s => s.Id == content.Section.Id);
-            content.Section.Id = currentSection.Id;
+            content.Section.Veteran = veteran;
 
             // find next Id for section (largest)
             int nextId = 0;
@@ -76,10 +80,12 @@ namespace MSR_Web_App.Controllers
 
             content.Id = nextId + 1;
 
+            content.FK_Veterans_Id = Int32.Parse(User.Identity.GetUserName());
+
             db.Contents.Add(content);
             db.SaveChanges();
 
-            return RedirectToAction("Portfolio", viewModel);
+            return RedirectToAction("PortfolioCreation", viewModel);
         }
 
         public ActionResult DeleteContent(Content content)
