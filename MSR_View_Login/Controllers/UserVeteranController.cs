1e38e460022c5d7973783bc206c8117f5a0087d2
Jackson Hales <me@jacksonhales.com.au>
2018-11-02 14:52:11 +1100

    Minor changes to content edit form

--- MSR_View_Login/Controllers/UserVeteranController.cs
+++ MSR_View_Login/Controllers/UserVeteranController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MSR_Web_App.Models;
 using Microsoft.AspNet.Identity;
+using System.IO;
 
 namespace MSR_Web_App.Controllers
 {
@@ -226,5 +227,28 @@ namespace MSR_Web_App.Controllers
             return RedirectToAction("PortfolioCreation", viewModel);
         }
 
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/images/profile"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+
+            }
+            // after successfully uploading redirect the user
+            return RedirectToAction("PortfolioCreation", viewModel);
+        }
     }
 }
