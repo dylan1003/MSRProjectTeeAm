ce1caf7dd719cab5533ed0acf891054d4eb5a6c2
DOMINIC POWELL <101614855@student.swin.edu.au>
2018-11-15 08:33:58 +1100

    changed welcome message to full name of veteran

---
+++ MSR_View_Login/Classes/VeteranName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MSR_Web_App.Models;
+
+namespace MSR_Web_App.Classes
+{
+    public class VeteranName
+    {
+        private Msr_Database_Release_TwoEntities db = new Msr_Database_Release_TwoEntities();
+
+        public string GetFullName(string Username)
+        {
+            Veteran veteran = db.Veterans.Find(Int32.Parse(Username));
+
+            var fullName = veteran.FirstName + ", " +  veteran.Surname;
+
+            return fullName;
+        }
+    }
+    
+}
